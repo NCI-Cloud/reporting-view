f69edbce520de2739e70c9b902785b27f4c51693
Malcolm Gill <malcolm.gill@gmail.com>
2015-07-22 10:14:14 +1000

    Fix whitespace (according to PEP 8).

--- front_end/urls.py
+++ front_end/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.base, name="base"),
-	url(r'^project/(?P<project_id>[0-9a-z-]*)', views.project, name="project"),
+    url(r'^$', views.base, name="base"),
+    url(r'^project/(?P<project_id>[0-9a-z-]*)', views.project, name="project"),
 ]
