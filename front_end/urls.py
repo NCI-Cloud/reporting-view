bd7b7b2df50e82370bc5bf52610d103166c12fb2
Malcolm Gill <malcolm.gill@gmail.com>
2015-08-21 15:55:17 +1000

    Restructure django stuff to serve >1 report.
    The future is now.

--- front_end/urls.py
+++ front_end/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import url
-
 from . import views
-from . import sqldump
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^dump/', sqldump.dump, name='dump'),
+    url(r'^(?P<report>\w+)/$', views.report),
 ]
--- front_end/views.py
+++ front_end/views.py
@@ -1,33 +1,12 @@
+from django import template
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.http import Http404
 
-def base(request):
-    template = loader.get_template('front_end/overview.html')
-    context = RequestContext(request, {
-        'overview': Overview(),
-    })
-    return HttpResponse(template.render(context))
-
-def project(request, project_id):
-    conn = sqlite3.connect(dbfile)
-    conn.row_factory = sqlite3.Row
-    template = loader.get_template('front_end/project.html')
-    p = Project.from_db(conn, project_id)
-    p.instances_from_db(conn)
-    p.get_aggregates()
-    context = RequestContext(request, {
-        'project': p,
-    })
-    return HttpResponse(template.render(context))
-
-def index(request):
-    from sqldump.models import Query
-    return render(
-        request,
-        'front_end/index.html',
-        context = {
-            'preload' : ['projects'],
-            'queries' : ['live_instances']
-        }
-    )
+# just casually using django to serve (almost) static html
+def report(request, report):
+    report = 'front_end/{}.html'.format(report)
+    try:
+        template.loader.get_template(report)
+        return render(request, report)
+    except template.TemplateDoesNotExist:
+        raise Http404('oops')
