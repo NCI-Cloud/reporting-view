6144c60b51499a4080bcbf6a6a1e20ae7ebcc2f2
Malcolm Gill <malcolm.gill@gmail.com>
2015-11-12 10:39:24 +1100

    Remove trailing slash from baseURL.
    To fix redundant slashes in concatenated urls.

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
 // idk if this is a silly design but it was the least bad that sprung to mind...
 // will eventually want to add some kind of abstraction for virtual endpoints, in order to define 'All nodes' endpoint with aggregation which would be defined per report
 var Config = {
-    baseURL : 'http://example.com/',
+    baseURL : 'http://example.com',
     tokenKey : 'token', // key in sessionStorage for keystone token
     flashKey : 'flash', // key in sessionStorage for temporary message storage
     endpoint : 'http://example.com:9495', // reporting api base url
